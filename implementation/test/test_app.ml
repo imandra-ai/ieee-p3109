75c0b45cb9a702a310d92d5ecf209253a4f0b405
Christoph M. Wintersteiger <christoph@imandra.ai>
2024-11-11 15:02:00 +0000

    Adjust to new formalisation

--- implementation/test/test_app.ml
+++ implementation/test/test_app.ml
@@ -6,17 +6,17 @@ let some_check = Float8.nan = Float8.ninf;;
 
 Printf.printf "NaN = -oo: %b\n" some_check
 
-let x = Float8.negate Float8.pinf B8P3;;
+let x = Float8.negate B8P3 Float8.pinf;;
 
 Printf.printf "+oo = %s\n"
-  (Float8.to_string Float8.pinf Specification.Format.B8P3)
+  (Float8.to_string Specification.Format.B8P3 Float8.pinf)
 ;;
 
 Printf.printf "-oo = %s\n"
-  (Float8.to_string Float8.ninf Specification.Format.B8P3)
+  (Float8.to_string Specification.Format.B8P3 Float8.ninf)
 ;;
 
-Printf.printf "(- +oo) = %s\n" (Float8.to_string x Specification.Format.B8P3);;
+Printf.printf "(- +oo) = %s\n" (Float8.to_string Specification.Format.B8P3 x);;
 
 Printf.printf "NaN = NaN: %b\n" (Float8.nan = Float8.nan)
 
@@ -24,5 +24,5 @@ let fmt = Specification.Format.B8P3
 
 let x = Float8.of_int_bitwise Z.one;;
 
-Printf.printf "-(%s) = %s\n" (Float8.to_string x fmt)
-  (Float8.to_string (Float8.negate x fmt) fmt)
+Printf.printf "-(%s) = %s\n" (Float8.to_string fmt x)
+  (Float8.to_string fmt (Float8.negate fmt x))
--- specification/test/test_app_spec.ml
+++ specification/test/test_app_spec.ml
@@ -9,60 +9,108 @@ open Specification
 
 let some_check = Float8.nan = Float8.ninf;;
 
-Printf.printf "NaN = -oo: %b\n" some_check
+Printf.printf "NaN = -oo: %b\n" some_check;;
 
-let x = Float8.negate Float8.pinf Format.B8P3;;
+Printf.printf "+oo = %s\n" (Float8.to_string Format.B8P3 Float8.pinf);;
 
-Printf.printf "+oo = %s\n" (Float8.to_string Float8.pinf Format.B8P3);;
+Printf.printf "-oo = %s\n" (Float8.to_string Format.B8P3 Float8.ninf)
 
-Printf.printf "-oo = %s\n" (Float8.to_string Float8.ninf Format.B8P3);;
+let neg_pinf = Float8.negate Format.B8P3 Float8.pinf;;
 
-Printf.printf "(- +oo) = %s\n" (Float8.to_string x Format.B8P3);;
+Printf.printf "(- +oo) = %s\n" (Float8.to_string Format.B8P3 neg_pinf);;
 
 Printf.printf "NaN = NaN: %b\n" (Float8.nan = Float8.nan)
 
-let chk_neg x fmt =
-  Printf.printf "-(%s) = %s\n" (Float8.to_string x fmt)
-    (Float8.to_string (Float8.negate x fmt) fmt)
+let chk_neg x f =
+  Printf.printf "-(%s) = %s\n" (Float8.to_string f x)
+    (Float8.to_string f (Float8.negate f x))
 
 let _ =
-  chk_neg (Float8.of_int_bitwise Z.one) Format.B8P3;
-  chk_neg (Float8.of_int_bitwise (Z.of_int 0x40)) Format.B8P3;
-  chk_neg (Float8.of_int_bitwise (Z.of_int 0x7E)) Format.B8P3;
-  chk_neg (Float8.of_int_bitwise (Z.of_int 0xFA)) Format.B8P3;
-  chk_neg (Float8.of_int_bitwise (Z.of_int 0x78)) Format.B8P3;
-  chk_neg (Float8.of_int_bitwise (Z.of_int 0xBD)) Format.B8P3;
-  chk_neg (Float8.of_int_bitwise (Z.of_int 0x3B)) Format.B8P3;
-  chk_neg (Float8.of_int_bitwise (Z.of_int 0xFD)) Format.B8P2
+  chk_neg (Float8.of_int_repr Z.zero) Format.B8P3;
+  chk_neg (Float8.of_int_repr Z.one) Format.B8P3;
+  chk_neg (Float8.of_int_repr (Z.of_int 0x40)) Format.B8P3;
+  chk_neg (Float8.of_int_repr (Z.of_int 0x7E)) Format.B8P3;
+  chk_neg (Float8.of_int_repr (Z.of_int 0xFA)) Format.B8P3;
+  chk_neg (Float8.of_int_repr (Z.of_int 0x78)) Format.B8P3;
+  chk_neg (Float8.of_int_repr (Z.of_int 0xBD)) Format.B8P3;
+  chk_neg (Float8.of_int_repr (Z.of_int 0x3B)) Format.B8P3;
+  chk_neg (Float8.of_int_repr (Z.of_int 0xFD)) Format.B8P2
 
-let chk_add x y fmt =
-  Printf.printf "(%s + %s) = %s\n" (Float8.to_string x fmt)
-    (Float8.to_string y fmt)
-    (Float8.to_string
-       (Float8.add x y fmt fmt fmt SaturationMode.OvfSat RoundingMode.TowardZero)
-       fmt)
+let chk_add f x y =
+  Printf.printf "(%s + %s) = %s\n" (Float8.to_string f x) (Float8.to_string f y)
+    (Float8.to_string f (Float8.add f f f (true, RoundingMode.TowardZero) x y))
 
 let _ =
-  let one = Float8.of_int_bitwise (Z.of_int 0x40) in
-  let two = Float8.of_int_bitwise (Z.of_int 0x44) in
-  chk_add one one Format.B8P3;
-  chk_add one two Format.B8P3;
-  chk_add two one Format.B8P3;
-  chk_add Float8.pinf one Format.B8P3
+  let one = Float8.of_int_repr (Z.of_int 0x40) in
+  let two = Float8.of_int_repr (Z.of_int 0x44) in
+  chk_add Format.B8P3 one one;
+  chk_add Format.B8P3 one two;
+  chk_add Format.B8P3 two one;
+  chk_add Format.B8P3 Float8.pinf one
+
+let result_to_string (x : ('a, string) Result.t) ~(f : 'a -> string) : string =
+  match x with
+  | Error e -> Printf.sprintf "Error: %s" e
+  | Ok x -> Printf.sprintf "Ok %s" (f x)
+
+let nan_or_exreal_to_string (x : Float8.NaNOrExReal.t) =
+  match x with
+  | Float8.NaNOrExReal.NaN -> "NaN"
+  | Float8.NaNOrExReal.XR x -> ExReal.to_string x
 
 let _ =
   Printf.printf "(P1) 0xBB = %s\n"
-    (ExReal.to_string
-       (Float8.to_extended_real
-          (Float8.of_int_bitwise (Z.of_int 0xBB))
-          Format.B8P1))
+    (result_to_string ~f:nan_or_exreal_to_string
+       (Float8.decode Format.B8P1 (Float8.of_int_repr (Z.of_int 0xBB))))
 
-let chk_add_scaled x y xscale yscale fmt =
-  Printf.printf "(%s * 2^%s + %s * 2^%s) = %s\n" (Float8.to_string x fmt)
-    (Z.to_string xscale) (Float8.to_string y fmt) (Z.to_string yscale)
-    (Float8.to_string
-       (Float8.add_scaled x y fmt fmt xscale yscale fmt SaturationMode.Other
-          RoundingMode.TowardPositive)
-       fmt)
+let chk_add_scaled x y s_x s_y f =
+  Printf.printf "(%s * 2^%s + %s * 2^%s) = %s\n" (Float8.to_string f x)
+    (Z.to_string s_x) (Float8.to_string f y) (Z.to_string s_y)
+    (Float8.to_string f
+       (Float8.add_scaled f f f
+          (false, RoundingMode.TowardPositive)
+          x s_x y s_y))
 
 let _ = chk_add_scaled Float8.pinf Float8.pinf Z.zero Z.zero Format.B8P1
+
+let print_decode x =
+  let f = Format.B8P3 in
+  let fx = Float8.of_int_repr (Z.of_int x) in
+  let dx = Float8.decode f fx in
+  Printf.printf "decode(%s) = %s\n" (Float8.to_string f fx)
+    (result_to_string ~f:nan_or_exreal_to_string dx)
+
+let _ =
+  print_decode 0x00;
+  print_decode 0x01;
+  print_decode 0x80;
+  print_decode 0xC0
+
+let _ =
+  let f = Format.B8P3 in
+  let x = Float8.NaNOrExReal.(XR (ExReal.of_int (Z.of_int (-1)))) in
+  let ex = Float8.encode f x in
+  Printf.printf "encode(%s) = %s\n"
+    (nan_or_exreal_to_string x)
+    (result_to_string ~f:(Float8.to_string f) ex)
+
+let _ =
+  let x = Q.of_int 0 in
+  Printf.printf "x = %s\n" (Q.to_string x);
+  let p = 3 in
+  Printf.printf "p = %d\n" p;
+  let b = 16 in
+  Printf.printf "b = %d\n" b;
+  let e = max (Util.floor_log2_abs x (Z.of_int 64)) (Z.of_int (1 - b)) in
+  Printf.printf "E = %s\n" (Z.to_string e);
+  let open ExReal.ResultInfix in
+  let two = Z.of_int 2 in
+  let s = rx x *. (two ^. Z.neg e) *. (two ^. Z.sub (Z.of_int p) Z.minus_one) in
+  Printf.printf "S = %s\n" (result_to_string ~f:ExReal.to_string s);
+  match s with
+  | Ok (R s) ->
+    let s = Q.to_int s in
+    let t = s mod Z.to_int (Util.ipow2 (Z.of_int (p - 1))) in
+    Printf.printf "T = %d\n" t
+  | Error e -> Printf.printf "error: %s" e
+  | _ -> Printf.printf "Non-R"
